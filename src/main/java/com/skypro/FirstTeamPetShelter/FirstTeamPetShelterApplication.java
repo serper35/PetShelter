8090eeac933a7c02279eeb0de7b0a9eaac86d6d8
VGAliyev <v.aliev84@yandex.ru>
2024-04-03 18:25:24 +0400

    Initial commit

---
+++ src/main/java/com/skypro/FirstTeamPetShelter/FirstTeamPetShelterApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.FirstTeamPetShelter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstTeamPetShelterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstTeamPetShelterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skypro/FirstTeamPetShelter/FirstTeamPetShelterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.FirstTeamPetShelter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstTeamPetShelterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
