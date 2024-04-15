bc4898776f2f9ebc9d7e3cae09789cc677a5c4a8
Artur Vakhitov <arthurvakhitov@gmail.com>
2024-04-15 21:29:11 +0300

    Add @Repository annotation

--- src/main/java/com/skypro/FirstTeamPetShelter/repository/PetServiceRepository.java
+++ src/main/java/com/skypro/FirstTeamPetShelter/repository/PetServiceRepository.java
@@ -2,7 +2,9 @@ package com.skypro.FirstTeamPetShelter.repository;
 
 import com.skypro.FirstTeamPetShelter.model.Pet;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PetServiceRepository extends JpaRepository<Pet, Long> {
 
 }
