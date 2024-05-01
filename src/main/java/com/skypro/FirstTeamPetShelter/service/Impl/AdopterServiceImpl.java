85ad93aa7acb8b9cc18522de51a4d0151c9d886a
VGAliyev <v.aliev84@yandex.ru>
2024-05-02 00:11:19 +0400

    Создание AdopterService и начало разработки его имплементации

---
+++ src/main/java/com/skypro/FirstTeamPetShelter/service/Impl/AdopterServiceImpl.java
@@ -0,0 +1,39 @@
+package com.skypro.FirstTeamPetShelter.service.Impl;
+
+import com.skypro.FirstTeamPetShelter.model.Adopter;
+import com.skypro.FirstTeamPetShelter.service.AdopterService;
+
+import java.util.Collection;
+import java.util.List;
+
+public class AdopterServiceImpl implements AdopterService {
+    @Override
+    public Adopter addAdopter(Adopter adopter) {
+        return null;
+    }
+
+    @Override
+    public Collection<Adopter> getAllAdopters() {
+        return List.of();
+    }
+
+    @Override
+    public Adopter getAdopter(long id) {
+        return null;
+    }
+
+    @Override
+    public Adopter editAdopter(Adopter adopter) {
+        return null;
+    }
+
+    @Override
+    public void deleteAdopter(long id) {
+
+    }
+
+    @Override
+    public Adopter getAdopterByTelegramId(long telegramId) {
+        return null;
+    }
+}
