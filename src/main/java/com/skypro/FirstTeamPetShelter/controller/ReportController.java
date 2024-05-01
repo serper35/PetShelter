6c2ab63b37ef48715a6fbd147f1a1ae7005fe245
VGAliyev <v.aliev84@yandex.ru>
2024-05-01 23:20:08 +0400

    ReportController, ReportRepository, ReportService, ReportServiceImpl добавлены

---
+++ src/main/java/com/skypro/FirstTeamPetShelter/controller/ReportController.java
@@ -0,0 +1,131 @@
+package com.skypro.FirstTeamPetShelter.controller;
+
+import com.skypro.FirstTeamPetShelter.model.Report;
+import com.skypro.FirstTeamPetShelter.service.ReportService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+import java.util.Date;
+
+@RestController
+@RequestMapping("report/")
+public class ReportController {
+    private final ReportService reportService;
+
+    public ReportController(ReportService reportService) {
+        this.reportService = reportService;
+    }
+
+    @PostMapping
+    public Report addReport(@RequestBody Report report) {
+        return reportService.addReport(report);
+    }
+
+    @Operation(
+            summary = "Получение отчета по ID",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Отчёт",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = Report.class)
+                            )
+                    )
+            },
+            tags = "Report"
+    )
+    @GetMapping("id/{id}")
+    public Report getReportById(@PathVariable(name = "ID отчёта") long id) {
+        return reportService.getReportById(id);
+    }
+
+    @Operation(
+            summary = "Получение списка отчетов по дате",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Отчёты по дате",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = Report[].class)
+                            )
+                    )
+            },
+            tags = "Report"
+    )
+    @GetMapping("date/{date}")
+    public Collection<Report> getReportsByDate(@PathVariable(name = "Дата отчётов") Date date) {
+        return reportService.getReportsByDate(date);
+    }
+
+    @Operation(
+            summary = "Получение списка отчётов по усыновителю",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Отчёты по усыновителю"
+                    )
+            },
+            tags = "Report"
+    )
+    @GetMapping("adopter/{adopter_id}")
+    public Collection<Report> getReportsByAdopter(@PathVariable(name = "ID усыновителя") long adopter_id) {
+        return reportService.getReportsByAdopter(adopter_id);
+    }
+
+    @Operation(
+            summary = "Получение всех отчётов из БД",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Отчёты",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = Report[].class)
+                            )
+                    )
+            },
+            tags = "Report"
+    )
+    @GetMapping
+    public Collection<Report> getAllReports() {
+        return reportService.getAllReports();
+    }
+
+    @Operation(
+            summary = "Редактирование отчета по ID",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Редактируемый отчёт",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = Report.class)
+                            )
+                    )
+            },
+            tags = "Report"
+    )
+    @PutMapping("edit/{id}/{report}")
+    public Report editReport(@PathVariable long id, @PathVariable Report report) {
+        return reportService.editReport(id, report);
+    }
+
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Удаление отчёта по ID"
+            )
+    })
+    @DeleteMapping("delete/{id}")
+    public void deleteReport(@PathVariable long id) {
+        reportService.deleteReport(id);
+    }
+}
---
+++ src/main/java/com/skypro/FirstTeamPetShelter/service/Impl/ReportServiceImpl.java
@@ -0,0 +1,71 @@
+package com.skypro.FirstTeamPetShelter.service.Impl;
+
+import com.skypro.FirstTeamPetShelter.exception.ReportNotFoundException;
+import com.skypro.FirstTeamPetShelter.model.Report;
+import com.skypro.FirstTeamPetShelter.repository.ReportRepository;
+import com.skypro.FirstTeamPetShelter.service.ReportService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Date;
+
+@Service
+public class ReportServiceImpl implements ReportService {
+    final ReportRepository reportRepository;
+    final Logger logger = LoggerFactory.getLogger(ReportServiceImpl.class);
+
+    public ReportServiceImpl(ReportRepository reportRepository) {
+        this.reportRepository = reportRepository;
+    }
+
+    @Override
+    public Report addReport(Report report) {
+        logger.info("Сохранение отчёта с ID {}...", report.getId());
+        return reportRepository.save(report);
+    }
+
+    @Override
+    public Report getReportById(long id) {
+        return reportRepository.findById(id).orElseThrow(() -> {
+            logger.error("Невозможно получить отчёт! Данные Report с ID: {} не найден!", id);
+            return new ReportNotFoundException("Невозможно получить отчёт! Данные Report с ID: " + id + " не найден!");
+        });
+    }
+
+    @Override
+    public Collection<Report> getReportsByDate(Date date) {
+        return reportRepository.findAllByReportDate(date);
+    }
+
+    @Override
+    public Collection<Report> getReportsByAdopter(long adopter_id) {
+        return reportRepository.findAllByAdopterId(adopter_id);
+    }
+
+    @Override
+    public Collection<Report> getAllReports() {
+        return reportRepository.findAll();
+    }
+
+    @Override
+    public Report editReport(long id, Report report) {
+        return reportRepository.findById(id).map(
+                editableReport -> {
+                    editableReport.setReportDate(report.getReportDate());
+                    editableReport.setPetDiet(report.getPetDiet());
+                    editableReport.setPetPhoto(report.getPetPhoto());
+                    editableReport.setChangeBehavior(report.getChangeBehavior());
+                    editableReport.setPetHealthAndAdaptation(report.getPetHealthAndAdaptation());
+                    editableReport.setAdopter(report.getAdopter());
+                    return reportRepository.save(editableReport);
+                }).orElse(null);
+    }
+
+    @Override
+    public void deleteReport(long id) {
+        logger.warn("Удаляется отчёт с ID: {}", id);
+        reportRepository.deleteById(id);
+    }
+}
