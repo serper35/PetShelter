a8285344bc8047711c805621136926822f05d9e3
VGAliyev <v.aliev84@yandex.ru>
2024-05-02 23:37:31 +0400

    Передобавление в отслеживание GIT TelegramBotConfiguration

---
+++ src/main/java/com/skypro/FirstTeamPetShelter/configuration/TelegramBotConfiguration.java
@@ -0,0 +1,26 @@
+package com.skypro.FirstTeamPetShelter.configuration;
+
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.model.BotCommand;
+import com.pengrad.telegrambot.request.SetMyCommands;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TelegramBotConfiguration {
+    @Value("${telegram.bot.token}")
+    private String token;
+
+    @Bean
+    public TelegramBot telegramBot() {
+        TelegramBot bot = new TelegramBot(token);
+        BotCommand[] commands = {
+                new BotCommand("/start", "Начало работы с ботом"),
+                new BotCommand("/call-volunteer", "Позвать волонтёра"),
+                new BotCommand("/info", "Информация о боте")
+        };
+        bot.execute(new SetMyCommands(commands));
+        return bot;
+    }
+}
