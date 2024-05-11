8f58926f0c8c135f0a54cea090d28937f815dc00
VGAliyev <v.aliev84@yandex.ru>
2024-05-11 22:05:14 +0400

    Документирование BotService

--- src/main/java/com/skypro/FirstTeamPetShelter/service/bot/BotService.java
+++ src/main/java/com/skypro/FirstTeamPetShelter/service/bot/BotService.java
@@ -15,12 +15,74 @@ public interface BotService {
      * @return              - Возвращает Role
      */
     Role getVisitorRole(Long visitorId);
+
+    /**
+     * Отправка сообщения из Update
+     * @param telegramBot       - TelegramBot
+     * @param update            - Update
+     * @param messageText       - Сообщение
+     * @param menu              - Меню
+     */
     void sendResponseFromUpdate(TelegramBot telegramBot, Update update, String messageText, Menu menu);
+
+    /**
+     * Отправка сообщения из Callback
+     * @param telegramBot       - TelegramBot
+     * @param callbackQuery     - CallbackQuery
+     * @param messageText       - Сообщение
+     * @param menu              - Меню
+     * @param shelter           - Приют
+     */
     void sendResponseFromCallback(TelegramBot telegramBot, CallbackQuery callbackQuery, String messageText, Menu menu, Shelter shelter);
+
+    /**
+     * Отправить текстовое сообщение
+     * @param telegramBot       - TelegramBot
+     * @param id                - ID чата
+     * @param messageText       - Сообщение
+     * @param menu              - Меню
+     */
     void executeMessage(TelegramBot telegramBot, Long id, String messageText, Menu menu);
+
+    /**
+     * Отправить сообщение с изображением
+     * @param caption           - Подпись под изображением
+     * @param telegramBot       - TelegramBot
+     * @param callbackQuery     - CallbackQuery
+     * @param menu              - Меню
+     * @param Image             - Изображение (массив byte[])
+     * @param shelter           - Приют
+     */
     void executeImageMessage(String caption, TelegramBot telegramBot, CallbackQuery callbackQuery, Menu menu, byte[] Image, Shelter shelter);
+
+    /**
+     * Позвать волонтёра
+     * @param telegramBot       - TelegramBot
+     * @param update            - Update
+     */
     void callVolunteer(TelegramBot telegramBot, Update update);
+
+    /**
+     * Потенциальный усыновитель
+     * @param pet               - Pet
+     * @param telegramBot       - TelegramBot
+     * @param callbackQuery     - CallbackQuery
+     */
     void setAdoptiveParent(Pet pet, TelegramBot telegramBot, CallbackQuery callbackQuery);
+
+    /**
+     * Получить список питомцев
+     * @param telegramBot       - TelegramBot
+     * @param callbackQuery     - CallbackQuery
+     * @param shelter           - приют
+     */
     void getPets(TelegramBot telegramBot, CallbackQuery callbackQuery, Shelter shelter);
+
+    /**
+     * Установка номера телефона пользователю
+     * @param phone             - Номер телефона
+     * @param telegramBot       - TelegramBot
+     * @param update            - Update
+     */
     void setUserPhone(String phone, TelegramBot telegramBot, Update update);
 }
